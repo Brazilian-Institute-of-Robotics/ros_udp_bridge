0ed0547d93a90a6f3d790b94f0f7a40af42cf770
lumoreno17 <lumoreno.borges@gmail.com>
2018-12-10 10:56:04 -0300

    Add /include directory on ROS package for udp_class header file.

---
+++ include/udp_class.hpp
@@ -0,0 +1,33 @@
+#include "ros/ros.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <std_msgs/Char.h>
+#include <sstream>
+
+#define BUFF_SIZE 2048 
+#define PORT 1153
+
+class UDPReceive{
+    int port;
+    unsigned char buffer[BUFF_SIZE];
+    struct sockaddr_in myaddr;
+	struct sockaddr_in remaddr; 
+    socklen_t addrlen;
+    int recvlen;
+	int server;
+    std_msgs::Char msg;
+
+    public:
+        ros::NodeHandle n;
+        ros::Publisher pub;
+        UDPReceive(int port);
+        void start();
+};
+
+
--- src/udp_class.cpp
+++ src/udp_class.cpp
@@ -3,7 +3,7 @@
 UDPReceive::UDPReceive(int port)
 	: port(port)
 {  
-	//pub = n.advertise<std_msgs::Char>("udp_data", 10);
+	pub = n.advertise<std_msgs::Char>("udp_data", 10);
     
 	memset((char *)&myaddr, 0, sizeof(myaddr));
 	myaddr.sin_family = AF_INET;
@@ -29,8 +29,8 @@ void UDPReceive::start(){
 		if (recvlen > 0) {
 			buffer[recvlen] = 0; 
 			printf("received message: \"%s\"\n", buffer);
-			//msg.data = *buffer;
-			//pub.publish(msg);		
+			msg.data = *buffer;
+			pub.publish(msg);		
 		} 
 	} 
 
@@ -38,9 +38,9 @@ void UDPReceive::start(){
 
 int main(int argc, char **argv) {
 	int port = 1153;
-	//ros::init(argc, argv, "udp_comm_node");
+	ros::init(argc, argv, "udp_comm_node");
 	UDPReceive udp(1153);
 	udp.start();
-	//ros::spin();
+	ros::spin();
 	return 0;
 }
